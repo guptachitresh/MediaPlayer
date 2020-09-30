fe33d2827dfb11457793bde9c51224ed7adf188a
guptachitresh <guptachitresh99@gmail.com>
2020-10-01 02:11:45 +0530

    First commit

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private Button button;
+    private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar=(SeekBar) findViewById(R.id.seekBar);
+        mediaPlayer=new MediaPlayer();
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.paradise);
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        button=(Button)findViewById(R.id.playbutton);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mediaPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                    button.setText("Play");
+                }else{
+                    mediaPlayer.start();
+                    button.setText("Pause");
+                }
+            }
+        });
+
+    }
+}
